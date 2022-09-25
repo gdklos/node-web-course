2c01c3b1174f58f52dfb10b6d93ec5c71151c9a0
Gerardo Klos <gklos002@gmail.com>
2022-09-25 15:09:58 -0300

    heroku config

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => { 
    res.send('<h1>About</h1>')
 })
-
-app.listen(3000)
-console.log('server up on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log(`server up on port ${port}`)
